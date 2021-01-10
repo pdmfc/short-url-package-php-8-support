a5469d6789b203b465fc938880b9ca3ec345f2b8
João Santos <joao.santos@pdmfc.com>
2021-01-10 16:41:53 +0100

    add docblocks

--- src/Client/ShortUrlClient.php
+++ src/Client/ShortUrlClient.php
@@ -2,6 +2,7 @@
 
 namespace Pdmfc\Shorturl\Client;
 
+use stdClass;
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
 use Psr\Http\Message\ResponseInterface;
@@ -30,70 +31,82 @@ class ShortUrlClient
     }
 
     /**
-     * @param string $shortUrlCode
-     * @return mixed
+     * Fetches the short url data.
+     *
+     * @param string $uri
+     * @return \Illuminate\Http\Response|null|stdClass
      */
-    public function getUrl(string $shortUrlCode)
+    public function getUrl(string $uri)
     {
-        if (!$shortUrlCode) {
+        if (!$uri) {
             return response('Short Url Code is Required', Response::HTTP_FORBIDDEN);
         }
 
-        return $this->get("getShortUrl/{$shortUrlCode}");
+        return $this->get("getShortUrl/{$uri}");
     }
 
     /**
+     * Creates a short url.
+     *
      * @param array $params
-     * @return mixed
+     * @return null|stdClass
      */
-    public function createUrl(array $params = [])
+    public function createUrl(array $params = []): ?stdClass
     {
         return $this->post('createShortUrl', $params);
     }
 
     /**
-     * @param string $shortUrlCode
-     * @param array $params
-     * @return mixed
-     */
-    public function updateUrl(string $shortUrlCode, array $params)
-    {
-        if (!$shortUrlCode) {
-            return response('Short Url Code is Required', Response::HTTP_FORBIDDEN);
-        }
-
-        return $this->post("changeShortUrl/{$shortUrlCode}", $params);
-    }
-
-    /**
-     * @param $shortUrlCode
-     * @return mixed
-     */
-    public function deleteUrl(string $shortUrlCode)
-    {
-        if (!$shortUrlCode) {
-            return response('Short Url Code is Required', Response::HTTP_FORBIDDEN);
-        }
-
-        return $this->delete("deleteShortUrl/{$shortUrlCode}");
-    }
-
-    /**
+     * Updates the short url data.
+     *
      * @param string $uri
      * @param array $params
-     * @return mixed
+     * @return \Illuminate\Http\Response|null|stdClass
      */
-    protected function get(string $uri, array $params = [])
+    public function updateUrl(string $uri, array $params)
+    {
+        if (!$uri) {
+            return response('Short Url Code is Required', Response::HTTP_FORBIDDEN);
+        }
+
+        return $this->post("changeShortUrl/{$uri}", $params);
+    }
+
+    /**
+     * Deletes the short url.
+     *
+     * @param $uri
+     * @return \Illuminate\Http\Response|null|stdClass
+     */
+    public function deleteUrl(string $uri)
+    {
+        if (!$uri) {
+            return response('Short Url Code is Required', Response::HTTP_FORBIDDEN);
+        }
+
+        return $this->delete("deleteShortUrl/{$uri}");
+    }
+
+    /**
+     * Http client wrapper for sending a "GET" request.
+     *
+     * @param string $uri
+     * @param array $params
+     * @return null|stdClass
+     */
+    protected function get(string $uri, array $params = []): ?stdClass
     {
         return $this->sendRequest(Request::METHOD_GET, $uri, $params);
     }
 
     /**
+     * Http client wrapper for sending a "POST" request.
+     *
      * @param string $uri
      * @param array $params
-     * @return mixed
+     * @return null|stdClass
      */
-    protected function post(string $uri, array $params = [])
+    protected function post(string $uri, array $params = []): ?stdClass
     {
         return $this->sendRequest(Request::METHOD_POST, $uri, [
             RequestOptions::FORM_PARAMS => $params
@@ -101,24 +114,28 @@ class ShortUrlClient
     }
 
     /**
+     * Http client wrapper for sending a "DELETE" request.
+     *
      * @param string $uri
-     * @return mixed
+     * @return null|stdClass
      */
-    protected function delete(string $uri)
+    protected function delete(string $uri): ?stdClass
     {
         return $this->sendRequest(Request::METHOD_DELETE, $uri);
     }
 
     /**
-     * @param string $requestMethod
+     * Handles the http client request.
+     *
+     * @param string $method
      * @param string $uri
      * @param array $params
-     * @return mixed
+     * @return null|stdClass
      */
-    protected function sendRequest(string $requestMethod, string $uri, array $params = [])
+    protected function sendRequest(string $method, string $uri, array $params = []): ?stdClass
     {
         try {
-            $response = $this->client->request($requestMethod, trim($uri, '/'), $params);
+            $response = $this->client->request($method, trim($uri, '/'), $params);
         } catch (GuzzleException $exception) {
             $response = $exception->getResponse();
         }
@@ -128,9 +145,9 @@ class ShortUrlClient
 
     /**
      * @param ResponseInterface|null $response
-     * @return mixed
+     * @return null|stdClass
      */
-    protected function decodedResponse(?ResponseInterface $response)
+    protected function decodedResponse(?ResponseInterface $response): ?stdClass
     {
         $response = json_decode(optional(optional($response)->getBody())->getContents());
 
@@ -138,6 +155,8 @@ class ShortUrlClient
     }
 
     /**
+     * The ShortUrlClient base URI.
+     *
      * @return string
      */
     public function baseUri(): string
